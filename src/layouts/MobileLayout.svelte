e21433784930fc98abdcd05003bcd207ae2eaaf1
Samuel Andert <samuelandert@me.com>
2020-11-10 15:56:48 +0100

    fixed css layout bug

--- src/layouts/MobileLayout.svelte
+++ src/layouts/MobileLayout.svelte
@@ -2,8 +2,8 @@
   class="flex flex-col items-center justify-center h-full bg-white bg-center bg-cover"
   style="background-image: url(/images/background.webp)">
   <div
-    class="justify-center w-full max-w-lg bg-white shadow-2xl wrap md:m-12"
-    style="min-height:100%; max-height: 900px;">
+    class="justify-center w-full h-full max-w-lg bg-white shadow-2xl wrap md:m-12"
+    style="max-height: 900px;">
     <slot>
       <p>Dapp Screen</p>
     </slot>
