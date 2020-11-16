30daef7dd21b135e73df457504ac79194806aac3
jaensen <no@email.comm>
2020-11-16 14:45:02 +0100

    send xdai

--- src/dapps/wallet/processes/transferXDai/services/transferXDaiService.ts
+++ src/dapps/wallet/processes/transferXDai/services/transferXDaiService.ts
@@ -1,5 +1,5 @@
 import {TransferXDaiContext} from "../transferXDai";
 
 export const transferXDaiService = async (context:TransferXDaiContext) => {
-    return Promise.resolve();
+    return await context.safe.transferEth(context.account, context.transfer.value.data, context.transfer.recipient.data);
 }
