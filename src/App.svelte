b1132b8eb2201920d29f4e509cadc20d9cca4604
jaensen <no@email.comm>
2020-11-23 21:15:34 +0100

    Ask the user if he wants to cancel the running process when clicking in whitespace

--- src/App.svelte
+++ src/App.svelte
@@ -5,8 +5,8 @@
   import Router from "svelte-spa-router";
   import routes from "src/routes";
   import Tailwind from "src/Tailwind.svelte";
-  import { getLocaleFromNavigator, addMessages, init } from "svelte-i18n";
-  import { _ } from "svelte-i18n";
+  import {getLocaleFromNavigator, addMessages, init} from "svelte-i18n";
+  import {_} from "svelte-i18n";
 
   import omo_en from "src/dapps/omo/data/languages/en.json";
   import omo_de from "src/dapps/omo/data/languages/de.json";
@@ -21,15 +21,17 @@
   import Button from "./libs/o-views/atoms/Button.svelte";
   import ActionBar from "./libs/o-views/molecules/ActionBar.svelte";
   import Modal from "./libs/o-views/molecules/Modal.svelte";
-  import { OmoEvent } from "./libs/o-events/omoEvent";
-  import { RunProcess } from "./libs/o-events/runProcess";
+  import {OmoEvent} from "./libs/o-events/omoEvent";
+  import {RunProcess} from "./libs/o-events/runProcess";
   import Process from "./libs/o-views/molecules/Process.svelte";
-  import { onMount } from "svelte";
+  import {onMount} from "svelte";
   import Notifications from "./libs/o-views/molecules/Notifications.svelte";
 
   let safeAddress;
 
-  onMount(() => {});
+  onMount(() =>
+  {
+  });
 
   let notes_en = {
     notes_text:
@@ -53,25 +55,31 @@
   let actions = [];
 
   let isOpen = false;
+  let askForCancel = false;
+
   let runningProcess: Process = window.stateMachines.current();
 
   window.eventBroker
     .getTopic("omo", "shell")
-    .observable.subscribe((event: OmoEvent) => {
-      runningProcess = window.stateMachines.current();
-      if (event.type === "openMenu") {
-        isOpen = true;
-      }
-      if (event.type == "runProcess") {
-        runningProcess = window.stateMachines.run(
-          (<RunProcess>event).definition,
-          (<RunProcess>event).contextModifier
-        );
-        isOpen = true;
-      }
-    });
+    .observable.subscribe((event: OmoEvent) =>
+  {
+    runningProcess = window.stateMachines.current();
+    if (event.type === "openMenu")
+    {
+      isOpen = true;
+    }
+    if (event.type == "runProcess")
+    {
+      runningProcess = window.stateMachines.run(
+        (<RunProcess>event).definition,
+        (<RunProcess>event).contextModifier
+      );
+      isOpen = true;
+    }
+  });
 
-  function routeLoading(e) {
+  function routeLoading(e)
+  {
     safeAddress = localStorage.getItem("omo.safeAddress");
 
     if (!e.detail.userData) return;
@@ -84,7 +92,8 @@
 
   $: {
     let _quickActions = actions.filter((o) => o.pos && o.pos !== "overflow");
-    quickActions = [0, 1, 2, 3].map((index) => {
+    quickActions = [0, 1, 2, 3].map((index) =>
+    {
       let actionAt: any = _quickActions.find(
         (action) => action.pos == index + 1
       );
@@ -101,55 +110,79 @@
     console.log(overflowActions);
   }
 
-  function toggleOpen() {
+  function toggleOpen()
+  {
     isOpen = !isOpen;
   }
 
-  function modalWantsToClose() {
+  function modalWantsToClose()
+  {
     runningProcess = window.stateMachines.current();
-    if (runningProcess) {
-    } else {
+    if (runningProcess)
+    {
+      console.log("Cancel?!")
+      askForCancel = true;
+    }
+    else
+    {
       isOpen = false;
     }
   }
 </script>
 
-<Tailwind />
+<Tailwind/>
 
 <ComposeApp tw="font-primary bg-light-300">
   <Compose tw="mx-auto bg-light-100 w-full max-w-2xl">
     <Compose columns="1fr" rows="auto 1fr auto" tw="w-full">
       <Compose>
-        <Notifications />
+        <Notifications/>
       </Compose>
       <Compose rows="1fr" columns="1fr">
-        <Router {routes} on:routeLoading={routeLoading} />
+        <Router {routes} on:routeLoading={routeLoading}/>
       </Compose>
       <Compose>
         <ActionBar
           bind:safeAddress
           on:actionButtonClick={toggleOpen}
-          {quickActions} />
+          {quickActions}/>
       </Compose>
       <Modal bind:isOpen on:closeRequest={modalWantsToClose}>
-        {#if runningProcess}
-          <Process
-            process={runningProcess}
-            on:stopped={() => {
-              isOpen = false;
-              runningProcess = null;
-            }} />
-        {:else}
-          {#each overflowActions as action}
-            <div class="w-full">
-              <div class="space-y-2">
-                <div on:click={() => window.dispatchShellEvent(action.event())}>
-                  <Button text={action.label} type="secondary" />
-                </div>
+        {#if askForCancel}
+          <div class="w-full">
+            <div class="space-y-2">
+              <div on:click={() => {
+                isOpen = false;
+                askForCancel = false;
+                runningProcess = null;
+              }}>
+                <button>Cancel</button>
+              </div>
+              <div on:click={() => askForCancel = false}>
+                <button>Resume</button>
               </div>
             </div>
-          {/each}
-          <ProcessNav bind:isOpen />
+          </div>
+        {:else}
+          {#if runningProcess}
+            <Process
+              process={runningProcess}
+              on:stopped={() => {
+                isOpen = false;
+                runningProcess = null;
+              }}/>
+          {:else}
+            {#each overflowActions as action}
+              <div class="w-full">
+                <div class="space-y-2">
+                  <div on:click={() => window.dispatchShellEvent(action.event())}>
+                    <Button text={action.label} type="secondary"/>
+                  </div>
+                </div>
+              </div>
+            {/each}
+            <ProcessNav bind:isOpen/>
+          {/if}
         {/if}
       </Modal>
     </Compose>
