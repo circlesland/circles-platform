f00fea77eb761a86940b457563492f110d105b3d
Samuel Andert <samuelandert@me.com>
2020-11-28 12:23:21 +0100

    minor odentity keys page fix

--- src/dapps/odentity/views/pages/Keys.svelte
+++ src/dapps/odentity/views/pages/Keys.svelte
@@ -2,7 +2,7 @@
   import CategoryTitle from "src/libs/o-views/atoms/CategoryTitle.svelte";
   import Compose from "src/libs/o-views/atoms/Compose.svelte";
   import AccessItem from "src/libs/o-views/molecules/AccessItem.svelte";
-  import { labelKeys, keys } from "./../../data/actions";
+  import { labelKeys, keys } from "./../../data/keys";
 </script>
 
 <Compose rows="1fr" columns="1fr" tw="px-4 lg:p-6">
