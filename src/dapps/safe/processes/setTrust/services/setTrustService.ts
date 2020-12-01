c0c992b86c194e473935d2fcf3cb3ddd9bebc0b8
jaensen <no@email.comm>
2020-12-01 19:48:29 +0100

    refactored the processContext.ts and abstracted the profile and key directories of the fission storage

--- src/dapps/safe/processes/setTrust/services/setTrustService.ts
+++ src/dapps/safe/processes/setTrust/services/setTrustService.ts
@@ -3,7 +3,7 @@ import {SetTrustContext} from "../setTrust";
 
 export const setTrustService = async (context:SetTrustContext) => {
     return await context.environment.eth.contracts.hub.setTrust(
-        context.environment.me.myAddress,
+        context.environment.me.myKey.privateKey,
         context.environment.me.mySafe,
         context.data.trustReceiver.value,
         new BN(100)
