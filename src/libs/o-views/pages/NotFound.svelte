02c0762774e00ad536e48b460829439bf4a211a6
Samuel Andert <samuelandert@me.com>
2020-12-02 11:31:10 +0100

    Small update to the not found page

--- src/libs/o-views/pages/NotFound.svelte
+++ src/libs/o-views/pages/NotFound.svelte
@@ -1,14 +1,17 @@
-<div
-  class="absolute flex items-end justify-center w-full h-full py-16 z-100 bg-light-100">
+<script lang="ts">
+  import Button from "../atoms/Button.svelte";
+</script>
+
+<div class="flex items-end justify-center w-full h-full">
   <div>
-    <div class="m-6 text-xl text-white uppercase font-title text-bold">
+    <div class="m-6 text-xl uppercase text-primary font-title text-bold">
       page not found
     </div>
     <div class="flex justify-center ">
-      <a
-        href="#/omoli/dapps"
-        class="px-4 py-2 text-center text-white uppercase border-2 border-white rounded-xl">go
-        back to home</a>
+      <a href="#/omoli/dapps">
+        <Button
+          mapping={{ data: { label: 'Back to home' }, design: { type: 'secondary' } }} />
+      </a>
     </div>
   </div>
 </div>
