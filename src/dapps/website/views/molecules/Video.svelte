6079bcdea66e0e1e75d70aa90444f00fab90a9e6
Samuel Andert <samuelandert@me.com>
2020-12-09 13:58:07 +0100

    Added vide placeholder image

--- src/dapps/website/views/molecules/Video.svelte
+++ src/dapps/website/views/molecules/Video.svelte
@@ -1,21 +1,8 @@
-<script>
-  import Button from "src/libs/o-views/atoms/Button.svelte";
-</script>
-
-<style>
-  .video {
-    height: 700px;
-  }
-</style>
-
 <div class="my-20 text-white bg-white">.</div>
-<div
-  class="flex items-center justify-center m-12 video bg-light-200 rounded-xl borer border-light-300">
+<div class="flex items-center justify-center m-12 ">
   <div>
-    <div class="my-4">Video Placeholder (coming soon)</div>
     <a href="#/lightpaper" class="my-4">
-      <Button
-        mapping={{ data: { label: 'Read the lightpaper instead' }, design: { type: 'secondary' } }} />
+      <img src="./images/video.png" class="w-full" alt="video placeholder" />
     </a>
   </div>
 </div>
