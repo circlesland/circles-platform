fafa9d35e3e1cdeba75670f9a3e0171a7cd53c13
jaensen <no@email.comm>
2021-01-30 17:00:49 +0100

    ${branch_name}-bla

--- generate_fission_yml.sh
+++ generate_fission_yml.sh
@@ -1,8 +1,2 @@
 branch_name=`git rev-parse --abbrev-ref HEAD`
-echo "ignore: []" >> omo-shell/fission.yaml
-if [ $branch_name == "main" ]; then
-  echo "url: omo.fission.app" >> omo-shell/fission.yaml
-else
-  echo "url: ${branch_name}-omo.fission.app" >> omo-shell/fission.yaml
-fi
-echo "build: ./public" >> omo-shell/fission.yaml
+yes "" | fission app register -n "${branch_name}-bla" -v
