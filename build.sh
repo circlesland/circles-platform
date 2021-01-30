6ec52294b5fda3d902e0ac07ab68384f43531801
jaensen <no@email.comm>
2021-01-30 14:30:40 +0100

    merde

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 echo "Installing build dependencies .."
 npm i
-npx lerna bootstrap
+npx --no-install lerna bootstrap || exit
 
 echo "Building 'webnative' .."
 cd packages/omo-webnative || exit
@@ -15,52 +15,52 @@ npx lerna bootstrap
 
 echo "Building 'omo-quirks' .."
 cd packages/omo-quirks || exit
-npx --no-install tsc
+npx --no-install tsc || exit
 cd .. || exit
 
 echo "Building 'omo-utils' .."
 cd omo-utils || exit
-npx --no-install tsc
+npx --no-install tsc || exit
 cd .. || exit
 
 echo "Building 'omo-events' .."
 cd omo-events || exit
-npx --no-install tsc
+npx --no-install tsc || exit
 cd .. || exit
 
 echo "Building 'omo-models' .."
 cd omo-models || exit
-npx --no-install tsc
+npx --no-install tsc || exit
 cd .. || exit
 
 echo "Building 'omo-circles' .."
 cd omo-circles || exit
-npx --no-install tsc
+npx --no-install tsc || exit
 cd .. || exit
 
 echo "Building 'omo-process' .."
 cd omo-process || exit
-npx --no-install tsc
+npx --no-install tsc || exit
 cd .. || exit
 
 echo "Building 'omo-kernel-interfaces' .."
 cd omo-kernel-interfaces || exit
-npx --no-install tsc
+npx --no-install tsc || exit
 cd .. || exit
 
 echo "Building 'omo-kernel' .."
 cd omo-kernel || exit
-npx --no-install tsc
+npx --no-install tsc || exit
 cd .. || exit
 
 echo "Building 'omo-fission' .."
 cd omo-fission || exit
-npx --no-install tsc
+npx --no-install tsc || exit
 cd .. || exit
 
 echo "Building 'omo-indexes' .."
 cd omo-indexes || exit
-npx --no-install tsc
+npx --no-install tsc || exit
 cd .. || exit
 
 echo "Building 'omo-shell' .."
@@ -68,4 +68,4 @@ cd omo-shell || exit
 npm run build
 cd ../.. || exit
 
-npx lerna bootstrap
+npx --no-install lerna bootstrap
