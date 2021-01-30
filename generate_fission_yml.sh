58f560053e7b31512aede125fc2b0eb8ebb262dd
jaensen <no@email.comm>
2021-01-30 17:07:14 +0100

    not ${branch_name}-bla except when 'main'

--- generate_fission_yml.sh
+++ generate_fission_yml.sh
@@ -1,6 +1,8 @@
 branch_name=`git rev-parse --abbrev-ref HEAD`
+echo "ignore: []" >> omo-shell/fission.yml
 if [ $branch_name == "main" ]; then
-  yes "" | fission app register -n "omo" -v
+  echo "url: omo.fission.app" >> omo-shell/fission.yml
 else
-  yes "" | fission app register -n "${branch_name}-bla" -v
+  echo "url: ${branch_name}-omo.fission.app" >> omo-shell/fission.yml
 fi
+echo "build: ./public" >> omo-shell/fission.yml
