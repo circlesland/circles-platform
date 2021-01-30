948f129185a3fd10c8e0f8daa20de7d2c4d77ce2
jaensen <no@email.comm>
2021-01-30 16:20:38 +0100

    added an exception for the 'main' branch

--- generate_fission_yml.sh
+++ generate_fission_yml.sh
@@ -1,4 +1,8 @@
 branch_name=`git rev-parse --abbrev-ref HEAD`
 echo "ignore: []" >> omo-shell/fission.yml
-echo "url: ${branch_name}-omo.fission.app" >> omo-shell/fission.yml
+if [ $branch_name == "main" ]; then
+  echo "url: omo.fission.app" >> omo-shell/fission.yml
+else
+  echo "url: ${branch_name}-omo.fission.app" >> omo-shell/fission.yml
+fi
 echo "build: ./public" >> omo-shell/fission.yml
