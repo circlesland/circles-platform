a040a90185892cde7958f1f663e999acbf2be8b6
Samuel Andert <samuelandert@me.com>
2021-02-13 14:48:43 +0100

    switching deploy pipeline to circles.land

--- generate_fission_yml.sh
+++ generate_fission_yml.sh
@@ -1,8 +1,8 @@
 branch_name=`git rev-parse --abbrev-ref HEAD`
 echo "ignore: []" >> fission.yaml
 if [ $branch_name == "main" ]; then
-  echo "url: omo.fission.app" >> fission.yaml
+  echo "url: circles.fission.app" >> fission.yaml
 else
-  echo "url: ${branch_name}-omo.fission.app" >> fission.yaml
+  echo "url: ${branch_name}-circles.fission.app" >> fission.yaml
 fi
 echo "build: ./circles-land/public" >> fission.yaml
