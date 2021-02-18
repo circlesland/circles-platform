f968377a9af07d86d64ec4c6b08e7254a290c611
jaensen <daniel@djanz.de>
2021-02-18 18:28:16 +0100

    deploy test #1

--- deploy_prisma.sh
+++ deploy_prisma.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-sed -i "s/REPLACE_ME_WITH_THE_CONNECTION_STRING/$DO_PGSQL_CONNECTIONSTRING/g" packages/omo-central/schema.prisma
+blubb="s/REPLACE_ME_WITH_THE_CONNECTION_STRING/$DO_PGSQL_CONNECTIONSTRING/g"
+sed -i $blubb packages/omo-central/schema.prisma
 cd packages/omo-central/
 npx prisma db push --preview-feature
