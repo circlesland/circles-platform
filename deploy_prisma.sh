2862059dffa2f52e7b19c80f0de627331531ab11
jaensen <daniel@djanz.de>
2021-03-03 17:51:53 +0100

    display prisma version in build

--- deploy_prisma.sh
+++ deploy_prisma.sh
@@ -3,4 +3,5 @@ sedArgument="s/REPLACE_ME_WITH_THE_CONNECTION_STRING/${DO_PGSQL_CONNECTIONSTRING
 cp -f packages/omo-central-server/schema_template.prisma packages/omo-central-server/schema.prisma
 sed -i "${sedArgument}" packages/omo-central-server/schema.prisma
 cd packages/omo-central-server/
+npx prisma --version
 npx prisma db push --preview-feature
