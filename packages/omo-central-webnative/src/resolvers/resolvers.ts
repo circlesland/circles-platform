a8a49efc0083294b6cd1fe4f142ff6b7a263857a
jaensen <daniel@djanz.de>
2021-03-03 16:17:57 +0100

    reduced the resolvers of omo-central-webnative to the client-essential functions. The rest will be implemented in omo-central-server.

--- packages/omo-central-webnative/src/resolvers/resolvers.ts
+++ packages/omo-central-webnative/src/resolvers/resolvers.ts
@@ -1,34 +1,6 @@
 import {EventBroker} from "omo-utils/dist/eventBroker";
-import {serverResolver} from "./queries/server";
 import {profilesResolver} from "./queries/profiles";
-import {contactsResolver} from "./queries/contacts";
-import {fissionRootResolver} from "./queries/fissionRoot";
-import {offersResolver} from "./queries/offers";
-import {conversationResolver} from "./queries/conversation";
-import {lockOfferResolver} from "./mutations/lockOffer";
-import {provePaymentResolver} from "./mutations/provePayment";
 import {upsertProfileResolver} from "./mutations/upsertProfile";
-import {createOfferResolver} from "./mutations/createOffer";
-import {unlistOfferResolver} from "./mutations/unlistOffer";
-import {sendMessageResolver} from "./mutations/sendMessage";
-import {markMessageAsReadResolver} from "./mutations/markMessageAsRead";
-import {messagesSubscription} from "./subscriptions/message";
-import {activitiesSubscription} from "./subscriptions/activities";
-import {messageSender} from "./edges/message/sender";
-import {messageRecipient} from "./edges/message/recipient";
-import {profileOffers} from "./edges/profile/offers";
-import {profileContacts} from "./edges/profile/contacts";
-import {profilePurchases} from "./edges/profile/purchases";
-import {profileActivities} from "./edges/profile/activities";
-import {contactAnchorProfile} from "./edges/contact/anchorProfile";
-import {contactProfile} from "./edges/contact/contactProfile";
-import {offerCreatedBy} from "./edges/offer/createdBy";
-import {offerPictures} from "./edges/offer/pictures";
-import {purchasedFrom} from "./edges/purchase/purchasedFrom";
-import {purchasedBy} from "./edges/purchase/purchasedBy";
-import {purchasedItem} from "./edges/purchase/purchasedItem";
-import {activitiesResolver} from "./queries/activitities";
-import {purchasesResolver} from "./queries/purchases";
 import {circlesWalletsResolver} from "./queries/circlesWallets";
 import {ownTokenResolver} from "./edges/circlesWallet/ownToken";
 import {tokensResolver} from "./edges/circlesWallet/tokens";
@@ -50,26 +22,25 @@ import {Resolvers} from "omo-central-interfaces/dist/types";
 const wnfs = new WnfsClient()
 const eventBroker = new EventBroker(); // TODO: Replace with IPFS PubSub?!
 
+// Everything that's commented out is currently implemented in
+// omo-central-server. In the long run this functionality should
+// move here too.
 export const resolvers: Resolvers = {
     Query: {
-        server: serverResolver,
+        // offers: offersResolver(wnfs),
+        // activities: activitiesResolver(wnfs),
+        // contacts: contactsResolver(wnfs),
+        // conversation: conversationResolver(wnfs),
+        // purchases: purchasesResolver(wnfs),
         profiles: profilesResolver(wnfs),
-        fissionRoot: fissionRootResolver(wnfs),
-        offers: offersResolver(wnfs),
-        activities: activitiesResolver(wnfs),
-        contacts: contactsResolver(wnfs),
-        conversation: conversationResolver(wnfs),
-        purchases: purchasesResolver(wnfs),
         circlesWallets: circlesWalletsResolver(wnfs)
     },
     Mutation: {
-        lockOffer: lockOfferResolver(wnfs),
-        provePayment: provePaymentResolver(wnfs),
+        //createOffer: createOfferResolver(wnfs),
+        //unlistOffer: unlistOfferResolver(wnfs),
+        //sendMessage: sendMessageResolver(wnfs, eventBroker),
+        //markMessageAsRead: markMessageAsReadResolver(wnfs),
         upsertProfile: upsertProfileResolver(wnfs),
-        createOffer: createOfferResolver(wnfs),
-        unlistOffer: unlistOfferResolver(wnfs),
-        sendMessage: sendMessageResolver(wnfs, eventBroker),
-        markMessageAsRead: markMessageAsReadResolver(wnfs),
         addCirclesWallet: addCirclesWalletResolver(wnfs),
         addCirclesToken: addCirclesTokenResolver(wnfs),
         addCirclesTrustRelation: addCirclesTrustRelationResolver(wnfs),
@@ -81,33 +52,33 @@ export const resolvers: Resolvers = {
             throw new Error(`NotImplemented`);
         },
     },
-    Subscription: {
-        activities: activitiesSubscription(eventBroker),
-        messages: messagesSubscription(eventBroker)
-    },
-    Message: {
-        sender: messageSender(wnfs),
-        recipient: messageRecipient(wnfs)
-    },
-    Profile: {
-        offers: profileOffers(wnfs),
-        contacts: profileContacts(wnfs),
-        purchases: profilePurchases(wnfs),
-        activities: profileActivities(wnfs)
-    },
-    Contact: {
-        anchorProfile: contactAnchorProfile(wnfs),
-        contactProfile: contactProfile(wnfs),
-    },
-    Offer: {
-        createdBy: offerCreatedBy(wnfs),
-        pictures: offerPictures(wnfs)
-    },
-    Purchase: {
-        purchasedFrom: purchasedFrom(wnfs),
-        purchasedBy: purchasedBy(wnfs),
-        purchasedItem: purchasedItem(wnfs)
-    },
+    // Subscription: {
+    //     activities: activitiesSubscription(eventBroker),
+    //     messages: messagesSubscription(eventBroker)
+    // },
+    // Message: {
+    //     sender: messageSender(wnfs),
+    //     recipient: messageRecipient(wnfs)
+    // },
+    // Profile: {
+    //     offers: profileOffers(wnfs),
+    //     contacts: profileContacts(wnfs),
+    //     purchases: profilePurchases(wnfs),
+    //     activities: profileActivities(wnfs)
+    // },
+    // Contact: {
+    //     anchorProfile: contactAnchorProfile(wnfs),
+    //     contactProfile: contactProfile(wnfs),
+    // },
+    // Offer: {
+    //     createdBy: offerCreatedBy(wnfs),
+    //     pictures: offerPictures(wnfs)
+    // },
+    // Purchase: {
+    //     purchasedFrom: purchasedFrom(wnfs),
+    //     purchasedBy: purchasedBy(wnfs),
+    //     purchasedItem: purchasedItem(wnfs)
+    // },
     CirclesWallet: {
         ownToken: ownTokenResolver(wnfs),
         tokens: tokensResolver(wnfs),
