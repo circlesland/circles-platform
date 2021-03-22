5f783c30633d9664be6b556bb6df0f3a0029a85b
jaensen <daniel@djanz.de>
2021-03-22 21:55:40 +0100

    simpler proxy config

--- generate_proxy_config.sh
+++ generate_proxy_config.sh
@@ -3,30 +3,32 @@ limit_req_zone $binary_remote_addr zone=zone1:10m rate=4r/s;
 EOF`
 
 TEMPLATE=`cat << EOF
-$RATE_LIMITING
+http {
+  $RATE_LIMITING
 
-server {
-  listen 443 ssl;
-  server_name $1;
+  server {
+    listen 443 ssl;
+    server_name $1;
 
-  ssl_certificate /tls/fullchain1.pem;
-  ssl_certificate_key /tls/privkey1.pem;
+    ssl_certificate /tls/fullchain1.pem;
+    ssl_certificate_key /tls/privkey1.pem;
 
-  location /graphql {
-    limit_req zone=zone1 burst=16 nodelay;
+    location /graphql {
+      limit_req zone=zone1 burst=16 nodelay;
 
-    proxy_set_header X-Forwarded-For \\$proxy_add_x_forwarded_for;
-    proxy_set_header X-Forwarded-Proto \\$scheme;
-    proxy_set_header X-Real-IP \\$remote_addr;
-    proxy_set_header Host \\$http_host;
-    proxy_set_header Cookie \\$http_cookie;
-    proxy_set_header Upgrade \\$http_upgrade;
-    proxy_set_header Connection "upgrade";
+      proxy_set_header X-Forwarded-For \\$proxy_add_x_forwarded_for;
+      proxy_set_header X-Forwarded-Proto \\$scheme;
+      proxy_set_header X-Real-IP \\$remote_addr;
+      proxy_set_header Host \\$http_host;
+      proxy_set_header Cookie \\$http_cookie;
+      proxy_set_header Upgrade \\$http_upgrade;
+      proxy_set_header Connection "upgrade";
 
-    proxy_pass $2;
-    proxy_http_version 1.1;
-    proxy_read_timeout 86400s;
-    proxy_send_timeout 86400s;
+      proxy_pass $2;
+      proxy_http_version 1.1;
+      proxy_read_timeout 86400s;
+      proxy_send_timeout 86400s;
+    }
   }
 }
 EOF`
